9471ba26e760aa0aac4052756e4401842c2af268
uknow <yunhoknight@naver.com>
2023-01-08 11:36:50 +0900

    first

--- src/IntermediateOperationExample.java
+++ src/IntermediateOperationExample.java
@@ -3,8 +3,5 @@ import java.util.List;
 
 public class IntermediateOperationExample {
     public static void main(String[] args) {
-        String[][] namesArray = new String[][]{{"박해커", "이자바"}, {"김코딩", "나박사"}};
-
-        Arrays.stream(namesArray).flatMap(Arrays::stream).forEach(System.out::println);
     }
 }
